3545d431b4f37de5f855b99f24630ed966877e94
shuvo-99 <anontoshuvo99@gmail.com>
2023-12-20 21:36:12 +0600

    backend initialize

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const expres = require("express");
+const cors = require("cors");
+const app = expres();
+const port = process.env.PORT || 5000;
+
+// ===== middleware =====
+app.use(cors());
+app.use(expres.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
